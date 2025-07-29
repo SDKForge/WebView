42632bcfb721700e50d87288fd54c2054f914c16
Volodymyr Nevmerzhytskyi <azazellj@gmail.com>
2025-07-29 23:06:20 +0300

    initial release: WebView

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -8,10 +8,10 @@ plugins {
 }
 
 android {
-    namespace = "dev.sdkforge.template.android"
+    namespace = "dev.sdkforge.webview.android"
     compileSdk = 36
     defaultConfig {
-        applicationId = "dev.sdkforge.template.android"
+        applicationId = "dev.sdkforge.webview.android"
         minSdk = 21
         targetSdk = 36
         versionCode = 1
