e0d01b0a078771d6c20ad62f97c8a26e54947a02
Volodymyr Nevmerzhytskyi <azazellj@gmail.com>
2025-08-06 20:48:52 +0300

    - sync template

--- app-shared/src/iosMain/kotlin/dev/sdkforge/webview/app/ComposeAppViewController.kt
+++ app-shared/src/iosMain/kotlin/dev/sdkforge/webview/app/ComposeAppViewController.kt
@@ -1,11 +1,14 @@
 package dev.sdkforge.webview.app
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.backhandler.BackHandler
 import androidx.compose.ui.window.ComposeUIViewController
+import dev.sdkforge.webview.ui.rememberWebViewController
 import kotlin.experimental.ExperimentalObjCName
 
-@OptIn(ExperimentalObjCName::class)
+@OptIn(ExperimentalObjCName::class, ExperimentalComposeUiApi::class)
 @Suppress("FunctionName")
 @ObjCName("create")
 fun ComposeAppViewController() = ComposeUIViewController(
@@ -13,7 +16,15 @@ fun ComposeAppViewController() = ComposeUIViewController(
         enforceStrictPlistSanityCheck = false
     },
 ) {
+    val webViewController = rememberWebViewController()
+
+    BackHandler(
+        enabled = webViewController.platformWebView.canGoBack(),
+        onBack = { webViewController.platformWebView.goBack() },
+    )
+
     App(
+        webViewController = webViewController,
         modifier = Modifier
             .fillMaxSize(),
     )
